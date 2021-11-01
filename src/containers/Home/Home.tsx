faff9865649c820ad84ea389a629f87e62c15af2
Lucas Tamir Faria <lucas.tamir@rarolabs.com.br>
2021-11-01 17:43:15 -0300

    feature: Home component create

---
+++ src/containers/Home/Home.tsx
@@ -0,0 +1,52 @@
+import { useContext } from 'react'
+import { useHistory } from 'react-router-dom'
+import illustrationImg from '../../assets/images/illustration.svg'
+import logoImg from '../../assets/images/logo.svg'
+import googleImg from '../../assets/images/google-icon.svg'
+import enterImg from '../../assets/images/enter.svg'
+import Button from '../../components/Button';
+import '../../styles/auth.scss'
+import { AuthContext } from '../../context/AuthContext/AuthContext'
+
+export const Home: React.FC = () => {
+  const history = useHistory();
+  const { user, signInWithGoogle } = useContext(AuthContext);
+
+  const handleCreateNewRoom = async () => {
+    if (!user) {
+      await signInWithGoogle();
+    }
+    
+    history.push('/room/new');
+  }
+
+  return (
+    <div id="page-auth">
+      <aside>
+        <img src={illustrationImg} alt="Ilustração simbolizando perguntas e respostas" />
+        <strong>Toda pergunta tem uma resposta.</strong>
+        <p>Aprenda e compartilhe conhecimento com outras pessoas</p>
+      </aside>
+      <main>
+        <div className="main-content">
+          <img src={logoImg} alt="Logomarca do site" />
+          <Button onClick={handleCreateNewRoom} className="create-room">
+            <img src={googleImg} alt="Logomarca do google" />
+            Crie sua sala com o Google
+          </Button>
+          <div className="separator">ou entre em uma sala</div>
+          <form>
+            <input
+              type="text"
+              placeholder="Digite o código da sala"
+            />
+            <Button type="submit">
+              <img src={enterImg} alt="Icone indicando entrada na sala" />
+              Entrar na sala
+            </Button>
+          </form>
+        </div>
+      </main>
+    </div>
+  )
+}
