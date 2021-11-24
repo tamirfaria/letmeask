de9c5a60008f39f9b6061277900c02db0c340631
Lucas Tamir Faria <lucas.tamir@rarolabs.com.br>
2021-11-24 17:19:14 -0300

    feature: switch implementation

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,19 @@
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import NewRoom from "./containers/NewRoom";
 import Home from "./containers/Home";
 import { AuthContextProvider } from './context/AuthContext/AuthContext';
+import Room from './containers/Room';
 
 function App() {
 
   return (
     <BrowserRouter>
       <AuthContextProvider>
-        <Route path="/" exact component={Home} />
-        <Route path="/room/new" component={NewRoom} />
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/rooms/new" component={NewRoom} />
+          <Route path="/rooms/:id" component={Room} />
+        </Switch>
       </AuthContextProvider>
     </BrowserRouter>
   );
